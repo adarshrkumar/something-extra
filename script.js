c90329042d49f8dcc288cde753fc45026f1138cf
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-26 12:46:04 -0700

    update index.html

---
+++ script.js
@@ -0,0 +1,20 @@
+var text = document.getElementById('#result').textContent
+
+var split = text.split('aagctt').join('A/AGCTT')
+
+var items = split.split('/')
+split = split.split('/').replace(' ')
+
+var counts = []
+items.forEach(function(i) {
+    counts.push(i.length)
+})
+
+var countStr = ''
+
+counts.forEach(function(c, i) {
+    if (i !== 0) countStr += ' | '
+    countStr += c
+})
+
+document.getElementById('#result').textContent = counts
