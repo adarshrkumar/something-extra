8662e730509fd135fd61feea6f9f1ef8f9290499
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-26 12:52:42 -0700

    update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ var text = document.getElementById('result').textContent
 var splitItems = text.split('aagctt').join('A/AGCTT')
 
 var items = splitItems.split('/')
-splitItems = splitItems.split('/').replace(' ')
+splitItems = splitItems.split('/').join(' ')
 
 var counts = []
 items.forEach(function(i) {
     counts.push(i.length)
 })
 
-var countStr = ''
+var countStr = `${item.length-1} | `
 
 counts.forEach(function(c, i) {
-    if (i !== 0) countStr += ' | '
+    if (i !== 0) countStr += ' - '
     countStr += c
 })
 
-document.getElementById('result').textContent = counts
+document.getElementById('result').textContent = countStr
